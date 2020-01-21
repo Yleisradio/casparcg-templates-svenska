4127b5c54244e4314c408ccb7d1c7598264aae5b
Petter West <petter.west@yle.fi>
2020-01-21 16:07:36 +0200

    add flockler template

---
+++ js/flockler.js
@@ -0,0 +1,159 @@
+// This CasparCG template is made for Svenska YLE by Niclas Hallgren 2018
+
+var data = "";
+var flocklerId = flocklerIdLast = 0;
+var f1Last = f2Last = f3Last = f4 = "";
+var f5 = 10;
+var f6 = false;
+var articles = tmpArticles = lastArticles = [];
+var intervalRun = false;
+var intervalId;
+var tickerOn = false;
+
+function autoFetchFlockler() {
+	// Function to fetch articles from Flockler every 20 seconds
+    if (flocklerId!=0 && intervalRun == false) {
+        intervalId = setInterval(function() {
+            fetchFlockler();
+        }, 20000);
+        fetchFlockler();
+        intervalRun = true;
+    }
+}
+
+function startTicker() {
+	// Function to start ticker
+    $('#webTicker').webTicker({
+        height:'57px',
+        speed: '125',
+        hoverpause: false,
+        duplicate: true
+    });
+}
+
+function fadeOutTicker(articles) {
+	// Function to fade out ticker
+    $( "#ticker" ).fadeTo( 250 , 0, function() {
+        replaceTicker(articles);
+    });
+}
+
+function fadeInTicker() {
+	// Function to fade in ticker
+	$( "#ticker" ).fadeTo( 250 , 1 );
+}
+
+function replaceTicker(articles){
+	// Function to update ticker content
+    $("#webTicker").webTicker('update',
+        articles,
+        'swap',
+        true,
+        false
+    );
+	// If no articles, loop an invisible space
+	if (articles != "<li>&nbsp;</li>") {
+		setTimeout(fadeInTicker, 250);
+	}
+    
+};
+
+function sanitizeData(str) {
+	// Function to sanitize user ticker input
+    str = str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+    return str.trim();
+}
+
+function sanitizeFlocklerData(str) {
+	// Function to sanitize Flockler data (remove time)
+    str = str.replace(/<p>/g, "").replace(/<\/p>/g, "");
+    pos = str.indexOf("</strong>") + 9;
+    //if (pos != -1 && pos < 30) {
+        str = str.substring(pos);
+    //}
+    if (f6) {
+		// If f6 is set to true, only output text between <strong></strong> tags
+		var count = (str.match(/<strong>/g) || []).length;
+        if (count == 0) {
+            return null;
+        } else {
+            var tmpStr = "";
+            var lastPos = 0;
+            for (i = 0; i < count; i++) {
+                var startPos = str.indexOf("<strong>", lastPos) + 8;
+                var endPos = str.indexOf("</strong>", lastPos);
+                tmpStr = tmpStr + " " + str.substring(startPos, endPos);
+                lastPos = endPos + 1;
+            }
+            return tmpStr.trim();
+        }
+    } else {
+        str = str.replace(/<strong>/g, "").replace(/<\/strong>/g, "");
+        return str.trim();
+    }
+}
+
+function articlesEqual(a1, a2) {
+	// Function to check if articles shown are the same as before
+    if (a1 === a2) return true;
+    if (a1 == null || a2 == null) return false;
+    if (a1.length != a2.length) return false;
+
+    for (var i = 0; i < a1.length; ++i) {
+        if (a1[i] !== a2[i]) return false;
+    }
+    return true;
+}
+
+function convertUserTicker(str) {
+	// Function to convert user ticker data to ticker friendly format
+    lastArticles = articles.slice();
+    tmpArticles = [];
+    articles = [];
+    var userData = str.split("|");
+    for (i = 0; i < userData.length; i++) {
+        tmpArticles.push('<li>' + sanitizeData(userData[i]) + '&nbsp;&nbsp;&vert;&nbsp;</li>');
+    }
+    // How many articles to show (defaults to 10 if no value set in client (f5))
+    articles = tmpArticles.slice(0, f5);
+    // If fetched articles not equal to last fetch, update ticker
+    if (!articlesEqual(articles, lastArticles)) { 
+        fadeOutTicker(articles);
+    }
+}
+
+function fetchFlockler() {
+	// Fetch Flockler data
+    lastArticles = articles.slice();
+    tmpArticles = [];
+    articles = [];
+    // Fetch data from Flockler, output error message to CasparCG log if not successful
+    $.getJSON('https://api.flockler.com/v1/sites/545/sections/' + flocklerId+ '/articles?fields=id,body')
+        .done(function( jsonData) {
+            $.each(jsonData, function(key,value) {
+                if (key == "articles") {
+                    for (j = 0; j < value.length; j++) {
+                        var sanitized = sanitizeFlocklerData(value[j].body);
+                        if (sanitized != null) {
+                            tmpArticles.push('<li>' + sanitized + '&nbsp;&nbsp;&vert;&nbsp;</li>');
+                        }
+                    }
+                }
+            });
+            // Reverse order, oldest article first
+            //tmpArticles = tmpArticles.reverse();
+            // How many articles to show (defaults to 10 if no value set in client (f5))
+            articles = tmpArticles.slice(0, f5);
+            // If fetched articles not equal to last fetch update ticker
+            if (!articlesEqual(articles, lastArticles)) { 
+                fadeOutTicker(articles);
+            }
+		})	
+        .fail(function( jqxhr, textStatus, error ) {
+			// Could not fetch from server, output error to CasparCG log
+            var d = Date(Date.now());
+            a = d.toString();
+            var err = textStatus + ", " + error;
+            console.log( a + " Flockler Request Failed: " + err );
+        });
+}
---
+++ js/ticker.js
@@ -0,0 +1,195 @@
+// This CasparCG template is made for Svenska YLE by Niclas Hallgren 2018
+
+// Template opacity
+var maxOpacity = 1;
+
+function play() {
+	// Wait 40ms to load fonts before executing play command
+	function delayed(){
+			autoFetchFlockler();
+			$( "#main" ).fadeTo( 500 , maxOpacity );
+	}
+	setTimeout(delayed, 40);
+}
+
+function stop() {
+	// Fade out everything on stop
+	$( "#main" ).fadeTo( 500 , 0, function() {
+		$("#webTicker").webTicker('stop');
+	});
+}
+
+function next() {
+	// The next function has no functionality in this template
+}
+
+function update(str) {
+	// This function is executed every time the tempalte is played or updated
+	var data = "";
+	$( ".ticker_bg" ).css({"visibility": "hidden"});
+	// Check if user input is empty
+	if (str != "" || str != "<templateData></templateData>") {
+		try {
+			// Input JSON, parse to data variable
+			data = JSON.parse(str);
+		} catch (e) {
+			data = "";
+			// Input not JSON
+		}
+	}
+	// If data not JSON, do nothing
+	if (data!="") {
+		if ((data.f4 == "" || typeof data.f4 == 'undefined') && (typeof data.f0 == 'undefined' || data.f0 == "" || data.f0 == 0)) {
+			$( ".ticker_bg" ).css({"visibility": "hidden"});
+		} else {
+			$( ".ticker_bg" ).css({"visibility": "visible"});
+		}
+		
+		// Check if f1 is set by user
+		if (data.f1 != "" && typeof data.f1 != 'undefined') {
+			var f1 = document.getElementById('f1_text');
+			// Check if f1 is visible
+			if (f1.innerHTML == "") {
+				// Set input and fade in
+				$(" .f1_bg ").css({"visibility": "visible"});
+				f1.innerHTML = f1Last = data.f1.trim();
+				$( "#f1_bg" ).fadeTo( 250, maxOpacity );
+			} else {
+				// If f1 visible, fade out, replace and fade in
+				if (data.f1 != f1Last){
+					$( "#f1_bg" ).fadeTo( 250 , 0, function() {
+						$(" .f1_bg ").css({"visibility": "visible"});
+						f1Last = f1.innerHTML = data.f1.trim();
+					});
+					$( "#f1_bg" ).fadeTo( 250 , maxOpacity );
+				} 
+			}
+		} else {
+			// If f1 is not set fade out and clear
+			$( "#f1_bg" ).fadeTo( 250 , 0, function() {
+				$( ".f1_bg" ).css({"visibility": "hidden"});
+				f1Last = "";
+			});
+		}
+
+		if (data.f2 != "" && typeof data.f2 != 'undefined') {
+			var f2 = document.getElementById('f2_text');
+			if (f2.innerHTML == "") {
+				$(" .f2_bg ").css({"visibility": "visible"});
+				f2.innerHTML = f2Last = data.f2.trim();
+				$( "#f2_bg" ).fadeTo( 250, maxOpacity );
+			} else {
+				if (data.f2 != f2Last){
+					$( "#f2_bg" ).fadeTo( 250 , 0, function() {
+						$(" .f2_bg ").css({"visibility": "visible"});
+						f2Last = f2.innerHTML = data.f2.trim();
+					});
+					$( "#f2_bg" ).fadeTo( 250 , maxOpacity );
+				} 
+			}
+		} else {
+			$( "#f2_bg" ).fadeTo( 250 , 0, function() {
+				$( ".f2_bg" ).css({"visibility": "hidden"});
+				f1Last = "";
+			});
+		}
+
+		if (data.f3 != "" && typeof data.f3 != 'undefined') {
+			var f3 = document.getElementById('f3_text');
+			if (f3.innerHTML == "") {
+				$(" .f3_bg ").css({"display": "block"});
+				$( ".f3_bg" ).css({"visibility": "visible"});
+				f3.innerHTML = f3Last = data.f3.trim();
+				$( "#f3_bg" ).fadeTo( 250, maxOpacity );
+			} else {
+				if (data.f3 != f3Last){
+					$( "#f3_bg" ).fadeTo( 250 , 0, function() {
+						f3Last = f3.innerHTML = data.f3.trim();
+					});
+					$( "#f3_bg" ).fadeTo( 250 , maxOpacity );
+				} 
+			}
+		} else {
+			$( "#f3_bg" ).fadeTo( 250 , 0, function() {
+				$(" .f3_bg ").css({"display": "none"});
+			});	
+			f3Last = "";
+		}
+
+		// Check if f5 is set and is a number (how many articles to show)
+		if (typeof data.f5 != 'undefined' && data.f5 != "" && parseInt(data.f5) != 0 && Number.isInteger(parseInt(data.f5.trim()))) {
+			f5 = parseInt(data.f5.trim());
+		} else {
+			// If f5 is not set, use 10 as default
+			f5 = 10;
+		}
+
+		// Check if f6 is set (if true the template will only show text within <strong></strong> tags)
+		if (typeof data.f6 != 'undefined' && data.f6 != "") {
+			// If true set as true else as false
+			if (data.f6 == "true") {
+				f6 = true;
+			} else if (data.f6 == "false") {
+				f6 = false;
+			}
+		} else {
+			f6 = false;
+		}
+
+		// Check if Flockler id is set, is a number and has not changed since last fetch from server
+		if (typeof data.f0 != 'undefined' && data.f0 != "" && data.f0 != 0 && Number.isInteger(parseInt(data.f0))) {
+			if (flocklerIdLast != data.f0.trim()) {
+				// Check if ticker is running, start if not
+				if (!tickerOn) {
+					startTicker();
+					tickerOn = true;
+				}
+				// Set Flockler id based on user input
+				flocklerId = flocklerIdLast = data.f0.trim();
+				// Start auto fetch from Flockler
+				autoFetchFlockler();
+			}
+		} else {
+			// Flockler id not set, clear old Flockler id and articles
+			flocklerId = flocklerIdLast = 0;
+			articles = [];
+			// Stop auto fetch from Flockler
+			intervalRun = false;
+			clearInterval(intervalId);
+			if (tickerOn) {
+				fadeOutTicker('<li>&nbsp;</li>');
+			}
+		}
+
+		// Is f4 set (user generated ticker content)
+		if (data.f4 != "" && typeof data.f4 !== 'undefined') {
+			// Is Flockler id 0 or unset
+			if (data.f0 == "" || data.f0 == 0 || typeof data.f0 == 'undefined') {
+				// Start ticker if not running
+				if (!tickerOn) {
+					startTicker();
+					tickerOn = true;
+				}
+				f4 = data.f4.trim();
+				// Convert user input to ticker data
+				convertUserTicker(f4);
+			} else {
+				// Flockler id is set do not use user ticker input
+				f4 = "";
+			}
+		} else {
+			// f4 not set
+			f4 = "";
+		}
+
+	} else {
+		// Template input not JSON or not set at all, do not show anything
+		$( ".f1_bg" ).css({"visibility": "hidden"});
+		$( ".f2_bg" ).css({"visibility": "hidden"});
+		$( ".f3_bg" ).css({"visibility": "hidden"});
+		$( ".ticker_bg" ).css({"visibility": "hidden"});
+		// Output error to CasparCG log (if set to log by CasparCG settings)
+		console.log("Warning! Input data not JSON!")
+	}
+
+}
